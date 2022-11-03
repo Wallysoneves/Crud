7fba36452236461eb4e197ece06defc31693cc24
Wallysoneves <wallysoneves.moraes@gmail.com>
2022-11-02 23:47:44 -0300

    função de listar, de back.

--- Crud/angular/src/app/courses/course-form/course-form.component.ts
+++ Crud/angular/src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,5 @@
+import { Location } from '@angular/common';
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -16,8 +18,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private service: CoursesService,
     private formBuilder: FormBuilder,
-    private snackBar: MatSnackBar
-  ) {
+    private snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category:[null]
@@ -29,11 +31,16 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-        .subscribe(result => console.log(result), error => this.onError('Erro ao salvar curso'));
+        .subscribe(result => this.onSucesso('Curso salvo com sucesso!'), error => this.onError('Erro ao salvar curso'));
+        this.onCancel();
   }
 
   onCancel() {
+    this.location.back();
+  }
 
+  private onSucesso(mensage: string) {
+    return this.snackBar.open(mensage,'', {duration: 5000});
   }
 
   private onError(mensage: string) {
--- Crud/spring/src/main/java/com/wallyson/crudspring/controller/CourseController.java
+++ Crud/spring/src/main/java/com/wallyson/crudspring/controller/CourseController.java
@@ -3,10 +3,15 @@ package com.wallyson.crudspring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wallyson.crudspring.models.Course;
@@ -28,6 +33,18 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    //@RequestMapping(method = RequestMethod.Post)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+       // System.out.println(course.getName());
+
+       //    return ResponseEntity.status(HttpStatus.CREATED.CREATED)
+       //                         .body(courseRepository.save(course));
+       
+       return courseRepository.save(course);
     }
 
     
